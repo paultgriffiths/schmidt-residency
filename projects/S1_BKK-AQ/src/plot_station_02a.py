d903161e3fd893ea6b72360f9697f92bd29b635b
Paul Griffiths <paul.griffiths@gmail.com>
2020-07-26 10:10:28 +0100

    figure for discussion with Ryan

--- projects/S1_BKK-AQ/src/plot_station_02a.py
+++ projects/S1_BKK-AQ/src/plot_station_02a.py
@@ -8,24 +8,28 @@ import pandas as pd
 import datetime
 import seaborn as sns
 import matplotlib
+import numpy as np
 # %%
 import socket
-print(socket.gethostname())
+host = socket.gethostname()
+if host=='atm-bromium.local':
+    disk='/Users/ptg21/Github/schmidt-residency/projects/S1_BKK-AQ/'
+datapath = disk + '' #FIXME
+figpath = disk + '/figs/'
+
 #%%%
 sns.set_style('darkgrid')
-# sns.set_context('talk')
-ax.yaxis.grid(True) # Show the horizontal gridlines
-ax.xaxis.grid(True) # Show the vertical gridlines
+
 
 #%%
 debug=False
 #%%
 matplotlib.rcParams['axes.grid'] = True
 matplotlib.rcParams['axes.grid.which'] = 'both'
-matplotlib.rcParams['xtick.minor.visible'] = True
+matplotlib.rcParams['xtick.minor.visible'] = False
 # %%
 # data processing on processed data - station data from another R script [TODO: add ref]
-station_02a  = feather.read_dataframe('../bkk_aq_df_processed_data/bkk_df1.feather')
+station_02a  = feather.read_dataframe(disk+'/bkk_aq_df_processed_data/bkk_df1.feather')
 station_02a.index = pd.to_datetime(station_02a.dtLong, format='%Y-%m-%d %H:%M:%S')
 station_02a.index = pd.Index.rename(station_02a.index, 'local_time')
 station_02a.drop('dtLong',axis=1, inplace=True)
@@ -35,24 +39,35 @@ if debug:
 #%%
 
 # %%
-fig, ax = plt.subplots(figsize=(20,5))
-station_02a['NO'].plot(ax=ax )# alpha=0.4
-ax.set_xlim([datetime.date(2014, 1, 1), datetime.date(2014, 12, 31)])
-ax.set_ylim(0,250)
+fig, ax = plt.subplots(figsize=(20,5), dpi=200.)
+station_02a['O3'].plot(ax=ax )# alpha=0.4
+ax.set_xlim([datetime.date(2014, 4, 1), datetime.date(2016, 4, 1)])
 ax.set_xlabel('Local Time')
 ax.set_ylabel('Ozone mixing ratio / ppbv')
+ax.set_ylim(0,125)
+# sns.set_context('talk')
+ax.yaxis.grid(True) # Show the horizontal gridlines
+ax.xaxis.grid(True) # Show the vertical gridlines
 
 # %%
 fig, ax = plt.subplots(figsize=(20,5))
-mm_02a_no = station_02a['NO'].groupby(by=[station_02a.index.year, station_02a.index.month])
-mm_02a_no.plot(ax=ax,lw=1)
-ax.set_xlim([datetime.date(2014, 1, 1), datetime.date(2014, 12, 31)])
+mm_02a_o3 = station_02a['O3'].groupby(by=pd.Grouper(freq="M"))
+mm_02a_o3.plot(ax=ax,lw=1)
+mm_02a_o3.aggregate({"O3":np.mean})['O3'].plot(ax=ax, lw=5, color='k')
+ax.set_xlim([datetime.date(2013, 7, 1), datetime.date(2016, 12, 30)])
+ax.set_xlabel('Local Time')
+ax.set_ylim(0,125)
+ax.set_ylabel('Ozone level / ppb')
+# sns.set_context('talk')
+ax.yaxis.grid(True) # Show the horizontal gridlines
+ax.xaxis.grid(True) # Show the vertical gridlines
+plt.title('BKK shows worse air quality in the dry season - Nov-Feb')
+plt.savefig(figpath+'/BKK_Station_02a_grouped_by_yrs_months.png')
 
 # %%
 # calculate trend in 95%-ile in monthly mean data
 # think this is a group by and then apply to each group and return in another DF
-mm_02a_no = station_02a['NO'].groupby(by=[station_02a.index.year, station_02a.index.month])
-mm_02a_no
-
-# something along lines of 
-#[station_02a['NO']> station_02a['NO'].quantile(0.995)].count()
+# this gets into terrible trouble when plotting as the x-axis is two-level, month and year, which 
+# is not a datetime object
+mm_02a_o3 = station_02a['O3'].groupby(by=[station_02a.index.year, station_02a.index.month])
+mm_02a_o3.unstack(level=0).plot() # for more details
