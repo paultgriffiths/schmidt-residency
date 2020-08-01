f8153b75730bcfd856706ca8628aada07e9a26c9
Paul Griffiths <paul.griffiths@gmail.com>
2020-08-01 12:13:53 +0100

    script to extract data from long entries in JSON

--- projects/S2_Climate_health_analysis/convert_json_to_dicts.py
+++ projects/S2_Climate_health_analysis/convert_json_to_dicts.py
@@ -1,16 +1,33 @@
 #%%
 import json
+import re
+import pandas as pd
 #%%
 with open("data_json/ESGF_results_2020-07-25.json", "r") as read_file:
     esgf_data = json.load(read_file)
 #%%
 # first look at this dictionary - they're nested
-esgf_data.keys()
+# esgf_data.keys()
 # an array of dictionaries
-esgf_data2 = esgf_data['response']['docs']
+esgf_holdings_master_list = pd.DataFrame.from_dict(esgf_data['response']['docs'])
+esgf_holdings_master_list.assign()
 
 # %%
-for ivar in range(0, len(esgf_data2)):
-    print(esgf_data2[ivar]['source_id'])
+# use re to split as split method takes only one split string
+def split_esgf_string(model_data):
+    model_data = re.split('\.|\|', esgf_holdings_master_list['id'].loc[1])
+    return model_data
+# %%
+props_of_interest = ['CMIP', 'MIP', 'Centre', 'Model', 'Ensemble', 'Experiment', 'Realm', 'Variable', 'Grid', 'Date']
+esgf_holdings=pd.DataFrame(columns= props_of_interest)
+for ivar,col_label in zip(range(0,len(props_of_interest)), props_of_interest):
+    # create a series that is the model ensemble and assign to the column of interest
+    esgf_holdings[col_label] = esgf_holdings_master_list.apply(lambda x: split_esgf_string(x)[ivar])
+esgf_holdings.set_index(esgf_holdings['Model'], drop=True, inplace=True)
+esgf_holdings.sample(5)
+
+
+# %%
+
 
 # %%
